0fc5d46b68cc92b4ea86b368124e15b0580ee062
viliusramulionis <35171606+viliusramulionis@users.noreply.github.com>
2021-10-01 20:50:46 +0300

    General fixes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const hbs = require('express-handlebars');
 const app = express();
 const path = require('path');
 const db = require('./db/connection');
+const validator = require('validator');
 
 app.use(express.urlencoded({
     extended: false
@@ -38,7 +39,24 @@ app.post('/add-company', (req, res) => {
     let companyName     = req.body.name;
     let companyAddress  = req.body.address;
 
+    if(!validator.isAlphanumeric(companyName, 'en-US', {ignore: ' .ąĄčČęĘėĖįĮšŠųŲūŪ'}) 
+        || !validator.isLength(companyName, {min: 3, max: 50})) {
+        res.redirect('/list-companies/?m=Įveskite kompanijos pavadinimą&s=danger'); 
+        return;
+    }
+
+    if(!validator.isAlphanumeric(companyAddress, 'en-US', {ignore: ' .ąĄčČęĘėĖįĮšŠųŲūŪ'}) 
+        || !validator.isLength(companyAddress, {min: 3, max: 100})) { 
+        res.redirect('/list-companies/?m=Įveskite kompanijos adresą&s=danger'); 
+        return;
+    }
+
     db.query(`SELECT * FROM companies WHERE name = '${companyName}'`, (err, resp) => {
+
+        if(err) {
+            res.redirect('/list-companies/?m=Įvyko klaida&s=danger');
+            return;
+        }
 
         if(resp.length == 0) {
             
@@ -50,11 +68,11 @@ app.post('/add-company', (req, res) => {
                     return;
                 }
 
-                res.redirect('/list-companies/?m=Sėkmingai pridėjote įrašą');
+                res.redirect('/list-companies/?m=Sėkmingai pridėjote įrašą&s=success');
             });
 
         } else {
-            res.redirect('/list-companies/?m=Toks įrašas jau egzistuoja');
+            res.redirect('/list-companies/?m=Toks įrašas jau egzistuoja&s=warning');
         }
 
     });
@@ -64,11 +82,12 @@ app.post('/add-company', (req, res) => {
 app.get('/list-companies', (req, res) => {
 
     let messages = req.query.m;
+    let status = req.query.s;
 
     db.query(`SELECT * FROM companies`, (err, resp) => {
         
         if(!err) {
-            res.render('list-companies', {companies: resp, messages});
+            res.render('list-companies', {companies: resp, messages, status});
         }
 
     });
