a4755a72086b0d656bd88af166fbd9d87dd8d640
viliusramulionis <35171606+viliusramulionis@users.noreply.github.com>
2021-10-04 19:45:54 +0300

    2021-10-04

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, '/views/'));
 
 app.set('view engine', 'hbs');
 
-app.use('/static', express.static('public'));
+app.use('/static', express.static('static'));
 app.use('/static/css', express.static( path.join( __dirname, 'node_modules/bootstrap/dist/css') ) );
 app.use('/static/js', express.static( path.join( __dirname, 'node_modules/bootstrap/dist/js') ) );
 
@@ -88,6 +88,95 @@ app.get('/list-companies', (req, res) => {
         
         if(!err) {
             res.render('list-companies', {companies: resp, messages, status});
+        }
+
+    });
+
+});
+
+app.get('/edit-company/:id', (req, res) => {
+
+    let id          = req.params.id;
+    let messages    = req.query.m;
+    let status      = req.query.s;
+
+    db.query(`SELECT * FROM companies WHERE id = ${id}`, (err, resp) => {
+        
+        if(!err) {
+            res.render('edit-company', {company: resp[0], messages, status});
+        }
+
+    });
+});
+
+app.post('/edit-company/:id', (req, res) => {
+
+    let id              = req.params.id;
+    let companyName     = req.body.name;
+    let companyAddress  = req.body.address;
+
+    if(!validator.isAlphanumeric(companyName, 'en-US', {ignore: ' .ąĄčČęĘėĖįĮšŠųŲūŪ'}) 
+        || !validator.isLength(companyName, {min: 3, max: 50})) {
+        res.redirect('/edit-company/' + id +'/?m=Įveskite kompanijos pavadinimą&s=danger'); 
+        return;
+    }
+
+    if(!validator.isAlphanumeric(companyAddress, 'en-US', {ignore: ' .ąĄčČęĘėĖįĮšŠųŲūŪ'}) 
+        || !validator.isLength(companyAddress, {min: 3, max: 100})) { 
+        res.redirect('/edit-company/' + id +'/?m=Įveskite kompanijos adresą&s=danger'); 
+        return;
+    }
+
+    db.query(`SELECT COUNT(*) kiekis FROM companies WHERE name = '${companyName}' AND id != ${id}`, (err, dbresp) => {
+    
+        if(!err) {
+
+            if(dbresp[0].kiekis == 0) {
+
+                db.query(`UPDATE companies SET name = '${companyName}', address = '${companyAddress}' WHERE id = ${id}`, (err, resp) => {
+        
+                    if(!err) {
+
+                        res.redirect('/list-companies/?m=Įrašas sėkmingai išsaugotas&s=success');
+
+                    } else {
+
+                        res.redirect('/list-companies/?m=Įvyko klaida&s=danger');
+
+                    }
+            
+                });
+
+            } else {
+
+                res.redirect('/edit-company/' + id + '/?m=Toks kompanijos pavadinimas jau įrašytas&s=warning');
+
+            }
+
+        } else {
+
+            res.redirect('/list-companies/?m=Įvyko klaida&s=danger');
+
+        }
+
+    });
+
+});
+
+app.get('/delete-company/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    db.query(`DELETE FROM companies WHERE id = ${id}`, (err, resp) => {
+
+        if(!err) {
+
+            res.redirect('/list-companies/?m=Įrašas sėkmingai ištrintas&s=success');
+
+        } else {
+
+            res.redirect('/list-companies/?m=Nepavyko ištrinti įrašo&s=danger');
+        
         }
 
     });
---
+++ static/js/common.js
@@ -0,0 +1,11 @@
+let deleteCompany = document.querySelectorAll('.btn-delete-company');
+
+deleteCompany.forEach(node => node.addEventListener("click", function(e) {
+
+    e.preventDefault();
+
+    if(confirm('Ar tikrai norite pašalinti šią kompaniją?')) 
+        return true;
+
+    return false;
+}));
